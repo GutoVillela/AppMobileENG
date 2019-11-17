51f0acb161412392144ef86d7d529eb0f503de8b
Caio Cesar <caio.oliver.paulino@hotmail.com>
2019-11-17 17:15:51 -0300

    Criada as páginas no View, falta definir os models conforme o banco e a utiliazção dos views

---
+++ APP_LAB_ENG_SW/APP_LAB_ENG_SW/Views/AtualizarMontadora.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace APP_LAB_ENG_SW.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AtualizarMontadora : ContentView
+    {
+        public AtualizarMontadora()
+        {
+            InitializeComponent();
+        }
+    }
+}
