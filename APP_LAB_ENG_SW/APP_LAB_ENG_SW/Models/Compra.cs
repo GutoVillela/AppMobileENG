f84f144030ff98672ef4d5ff80be5877ab982c74
Caio Cesar <caio.oliver.paulino@hotmail.com>
2019-11-21 01:32:14 -0300

    Criado os arquivos de acordo com o diagrama, podemos discutir os métodos, as alterações que precisam ser feitas a respeito.

---
+++ APP_LAB_ENG_SW/APP_LAB_ENG_SW/Models/Compra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APP_LAB_ENG_SW.Models
+{
+    class Compra
+    {
+        public Array carrinho { get; set; }
+        public DateTime agenda { get; set; }
+
+        //Métodos abaixo estão abertos a discussão para Inserir/Remover/Alterar
+
+        public void inserirNoCarrinho(){ }
+
+        public void removerDoCarrinho() { }
+
+        public void agendar( ) { }
+    }
+}
---
+++ APP_LAB_ENG_SW/APP_LAB_ENG_SW/Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APP_LAB_ENG_SW.Models
+{
+    class Produto
+    {
+
+        public int codProduto { get; set; }
+        public string nomeProduto { get; set; }
+        public double nome { get; set; }
+        public int qtdProduto { get; set; }
+
+        public void cadastrarProduto() { }
+
+        public void alterarProduto(){}
+
+        public void excluirProduto(){}
+
+}
+}
