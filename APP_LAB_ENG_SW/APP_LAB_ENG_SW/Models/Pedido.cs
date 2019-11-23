0cd249bbf850ff970e1d85139b6ee1089d4f7609
LUCAS PAES <lucaspaes55@gmail.com>
2019-11-23 15:52:07 -0300

    Alterações

--- APP_LAB_ENG_SW/APP_LAB_ENG_SW/Models/Pedido.cs
+++ APP_LAB_ENG_SW/APP_LAB_ENG_SW/Models/Pedido.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
+using SQLitePCL;
 
 namespace APP_LAB_ENG_SW.Models
 {
+    [Table("Pedidos")]
     class Pedido
     {
-    public string modelo_carro {get; set;}
-    public int protocolo_montadora {get; set;}
-    public DateTime dt_pedido {get; set;}
-    public DateTime dt_entrega {get; set;}
-    public DateTime dt_retirada {get; set;}
-    public Pessoa cliente {get; set;}
-    public string concessionaria {get; set;}
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+
+        public string modelo_carro {get; set;}
+
+        public int protocolo_montadora {get; set;}
+
+        public DateTime dt_pedido {get; set;}
+
+        public DateTime dt_entrega {get; set;}
+
+        public DateTime dt_retirada {get; set;}
+
+        public Pessoa cliente {get; set;}
+
+        [Column("Concessionaria")]
+        public string concessionaria {get; set;}
     }
 }
